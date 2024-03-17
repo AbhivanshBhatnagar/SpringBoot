1b42c9c07a8299d5c8f4db2c1cf6811d706c6664
AbhivanshBhatnagar <bhatnagar1102abhivansh@gmail.com>
2024-03-17 14:18:57 +0530

    project init and dependency added

---
+++ jwt-login-signup/src/main/java/com/example/jwtloginsignup/JwtLoginSignupApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtloginsignup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtLoginSignupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtLoginSignupApplication.class, args);
+	}
+
+}
---
+++ jwt-login-signup/src/test/java/com/example/jwtloginsignup/JwtLoginSignupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtloginsignup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtLoginSignupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
